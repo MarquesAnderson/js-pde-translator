ca921fa5ff3eb1e8cb7ff425aab2750e2e4289dc
Marques Anderson <41755462+MarquesAnderson@users.noreply.github.com>
2021-10-01 13:52:52 -0500

    Updating example text to translate

---
+++ testInOut/variableInput.js
@@ -0,0 +1,29 @@
+// comment 1
+var x = 1.;// coment2
+var word = "69";
+var flo = 7.6;
+var bool = true;
+var ar = [];
+var Bigger = new Big();
+var big2 =new Big();
+for(var i = 0; i < 50; i++){
+    var x = 0;
+    x++;
+    let y = 0;
+}
+function func1(){
+
+    let y = 0;
+    ellipse(y, 50, 50, 50);
+
+}
+
+function returnAthing(){
+
+    return true;
+
+}
+
+var doThing = function(){
+    return 3;
+}
